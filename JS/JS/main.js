7435776914dc76926a149b88869a4b4b3c00125f
azaliacallysta <callystaazalia0@gmail.com>
2025-03-19 11:44:10 +0700

    [FIX] PORTOFOLIO

---
+++ JS/JS/main.js
@@ -0,0 +1,11 @@
+//console.log ("halo dunia");
+//document. write ("Saya senang sekali");
+
+
+var nama; "azalia";
+console.log(nama);
+// nama = "aza";
+// console.log(nama);
+
+var nama1
+// const nama2
